5e5d690e597dcada4d044f0dec134bd104d6a9c6
minsu.lee <minsu.rob@gmail.com>
2025-06-21 14:32:30 +0900

    Reapply "add: nativecn 다크모드 init"
    This reverts commit 6bf0f784c83feb8a4579a8cceef0e256348548ae.

--- components/feed/SamplePostCard.tsx
+++ components/feed/SamplePostCard.tsx
@@ -1,6 +1,11 @@
-import { Image, View, Text } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { useTheme } from "../../provider/NativecnProvider";
+import { Button } from "../Button";
+import { SimpleCard } from "../Card";
 
 export function SamplePostCard() {
+  const { theme, toggleTheme, setTheme } = useTheme();
+
   return (
     <View className="bg-white dark:bg-zinc-900 rounded-xl p-4 mb-4 shadow-sm">
       <View className="flex-row items-center mb-2">
@@ -25,6 +30,18 @@ export function SamplePostCard() {
         <Text className="text-zinc-600 dark:text-zinc-400">💬 3</Text>
         <Text className="text-zinc-600 dark:text-zinc-400">🔁</Text>
       </View>
+      <TouchableOpacity onPress={toggleTheme}>
+        <Text className="mb-2 text-base text-zinc-800 dark:text-zinc-200">
+          <Text>Toggle theme (Current: {theme})</Text>
+        </Text>
+      </TouchableOpacity>
+      <Button label="Button">버튼</Button>
+      <SimpleCard
+        title="Accelerate UI"
+        description="Enter a new development experience"
+        content="Sleek, easy to use components to build your next app faster."
+        footer="Inspired by shadcn/ui"
+      />
     </View>
   );
 }
